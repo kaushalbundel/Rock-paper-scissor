3b5d7ea1afe6cbf96baf76e904d56ed55dca070b
kwehen <110314567+kwehen@users.noreply.github.com>
2023-08-11 11:48:40 -0400

    Create Rock-paper-scissors.py
    Basic Rock Paper Scissors game, can be modified to be more complete

---
+++ Rock-paper-scissors.py
@@ -0,0 +1,39 @@
+# Rock, Paper, Scissors in Python
+import random
+
+user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors. Best two out of three.\n")
+computer = random.choice(['r', 'p', 's'])
+
+computer_wins = 0
+user_wins = 0
+
+while user_wins < 3 and computer_wins < 3:
+    user = user.lower()
+    print(f"Computer chose {computer}")
+    
+    if user == computer:
+        print('It\'s a tie')
+        user_wins = user_wins
+    elif user == 'r' and computer == 's':
+        print('You won!')
+        user_wins += 1
+    elif user == 'p' and computer == 'r':
+        print('You won!')
+        user_wins += 1
+    elif user == 's' and computer == 'p':
+        print('You won!')
+        user_wins += 1
+    elif user not in ['r', 'p', 's']:
+        print('Invalid choice')
+    else:
+        print('You lost!')
+        computer_wins += 1
+
+    if user_wins == 3:
+        print('You won the game!')
+        break
+    if computer_wins == 3:
+        print('You lost the game!')
+        break
+    user = input("What's your next choice? \n")
+    computer = random.choice(['r', 'p', 's'])
